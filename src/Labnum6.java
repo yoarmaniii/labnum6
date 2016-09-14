c9e2dc02fc0bf8fe6d5134c4343d85b41797105b
Armani Raleigh <armanijolee@gmail.com>
2016-09-14 16:39:06 -0400

    lab 6, its printing at least

--- src/Labnum6.java
+++ src/Labnum6.java
@@ -10,28 +10,37 @@ public class Labnum6 {
 		
 		//Declare and initialize new user input
 		Scanner userInput = new Scanner (System.in);
-		
+		String input;
+
 		//Declare your variables
-		String input = userInput.nextLine();
-		String character = new String(input);
-		
 		//lower case transformation value
-		System.out.println("Return Value");
-		System.out.println(character.toLowerCase());
-		
-		//get first letter of each word
-		char firstLetter = input.charAt(0);
-		input = input.substring(0);
-		
-		//adding "ay" to words with consonats
-		input = input + firstLetter + "ay";
-		System.out.println(input);
+		//System.out.println("Return Value");
+		//System.out.println(character.toLowerCase());
 		
 		
+			input =userInput.nextLine();
 		
-		
+			if( input.startsWith("a") || input.startsWith("e") || input.startsWith("i") ||
+                input.startsWith("o") || input.startsWith("u"));
+		{
+			System.out.println(input + "way");
+			
+		} 
+	
+				
+		{ 	 
+			System.out.print(input.substring(1) + input.substring(0,1) + "ay");
+			input=userInput.next();
+			System.out.println("So you want to try another? Enter (Yes/Quit)");
+			input=userInput.nextLine();
+			
+		}
+            
+				
+		{	System.out.println();	
+			System.out.println("See you later!");
 		
 
-	}
-
+		}
+}
 }
